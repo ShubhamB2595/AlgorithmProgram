5d5d31a8ec68fe0f3fdcd0b41ab73185d8ef671f
ShubhamB2595 <shubhambhele95@gmail.com>
2021-11-01 01:48:16 +0530

    refactored to get prime with Palindrome and anagram

--- src/com/primeinRange/PrimeInRange.java
+++ src/com/primeinRange/PrimeInRange.java
@@ -29,10 +29,55 @@ public class PrimeInRange {
 					count = count + 1;
 			}
 			if(count == 2) {
-				System.out.println(i);
+				//System.out.println(i);
+				checkPalin(i);
 			}
 		}	
 	}
+
+	//method for the use in anagram operation
+	static void updateFreq(int n, int [] freq) {
+        while (n > 0) {
+            int digit = n % 10;
+            freq[digit]++;
+            n /= 10;
+        }
+    }
+
+	//method to check the anagram of two int values
+    static boolean areAnagrams(int a, int b) {
+        int [] freqA = new int[10];
+        int [] freqB = new int[10];
+
+        updateFreq(a, freqA);
+        updateFreq(b, freqB);
+
+        for (int i = 0; i < 10; i++) {
+            if (freqA[i] != freqB[i])
+                return false;
+        }
+        return true;
+    }
 	
+    //method to checking number is palindrome or not
+    public static void checkPalin(int num) {
+    	
+    	int rem, sum = 0;
+    	int temp = num; 
+    	
+    	while(num > 0) {  
+            rem = num % 10;   
+            sum = sum * 10 + rem;   
+  
+            num = num / 10;   
+        }  
+    	if(sum == temp) {
+    		System.out.println("Prime number: " + temp + " is Palindrome");
+    	}
+    	else {
+    		System.out.println("Prime number: " + temp + " is Not Palindrome");
+    	}
+    }
+    
 }
 
